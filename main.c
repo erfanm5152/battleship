e1ca447cff00c277c62700482c0a17df96eec558
erfanm5152 <erfanm5152@gmail.com>
2021-02-04 14:46:21 +0330

    تصحیح تابع ghabel_jaygozari_bodan_kashtiha و اضافه کردن تابع gharar_dadan_w_atraf_keshtiha و put_ships .

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 #define MAX_USER 100
 #define NAGHSHE 10
 #define MAX_SIZE_KASHTI 5
+
+void show11();
 int nobat;
 typedef struct {
     int size;
@@ -74,8 +76,188 @@ void create_board(player *bazikon){
     }
 }
 
-void put_ships(player*bazikon){
 
+
+bool ghabel_jaygozari_bodan_kashtiha(player*bazikon,int ebteda_satr,int ebteda_soton,int enteha_satr,int enteha_soton,int size){
+    if (bazikon->naghshe[ebteda_satr][ebteda_soton][0]=='W'||bazikon->naghshe[ebteda_satr][ebteda_soton][0]=='K'){return false;}
+    if (size==1){
+        if (ebteda_satr<0||ebteda_satr>9||ebteda_soton>9||ebteda_satr<0){return false;}
+        else{return true;}
+    }
+    if (bazikon->naghshe[enteha_satr][enteha_soton][0]=='W'||bazikon->naghshe[enteha_satr][enteha_soton][0]=='K'){return false;}
+    else{
+        if (ebteda_satr<0||ebteda_satr>9||ebteda_soton>9||ebteda_satr<0||enteha_satr<0||enteha_satr>9||enteha_soton<0||enteha_soton>9){return false;}
+        if ((ebteda_satr!=enteha_satr) && (enteha_soton!=ebteda_soton)){false;}
+        else{
+            if (ebteda_satr==enteha_satr){
+                if ((abs(enteha_soton-ebteda_soton))==size-1){
+                    return true;
+                }
+                else{return false;}
+            }
+            else if (ebteda_soton == enteha_soton){
+                if ((abs(enteha_satr-ebteda_satr))==size-1){
+                    return true;
+                }
+                else{return false;}
+            }
+        }
+    }
+}
+
+int minimum(int a,int b){
+    if (a>b){return b;}
+    else{return a;}
+}
+void gharar_dadan_w_atraf_keshtiha(player*bazikon){
+    for (int i = 0; i <NAGHSHE ; i++) {
+        for (int j = 0; j <NAGHSHE ; j++) {
+            if (bazikon->naghshe[i][j][0]=='K'){
+                for (int k = i-1; k <i+2 ; k++) {
+                    for (int l = j-1; l <j+2 ; l++) {
+                        if (k<0||k>9||l<0||l>9||bazikon->naghshe[k][l][0]=='K'){ continue;}
+                        else{bazikon->naghshe[k][l][0]='W';}
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+void vasl_kardan_keshtiha_be_linkedlist(player *bazikon,int size){
+
+}
+
+void put_ships(player*bazikon){
+    show11();
+    int adad_switch;
+    scanf("%d",&adad_switch);
+    getchar();
+    keshti *curr=bazikon->head;
+    int satr_ebteda,soton_ebteda,soton_enteha,satr_enteha;
+    switch (adad_switch) {
+        case 1:
+            break;
+        case 2:
+            for (int i = 0; i <4 ; i++) {
+                lab1:
+                print_naghshe(bazikon->naghshe,0);
+                printf("kashti be tol 1:\n");
+                printf("\tsatr: ");
+                scanf("%d",&satr_ebteda);
+                getchar();
+                printf("\tsoton: ");
+                scanf("%d",&soton_ebteda);
+                getchar();
+                if (ghabel_jaygozari_bodan_kashtiha(bazikon,satr_ebteda,soton_ebteda,satr_enteha,soton_enteha,1)){
+                    bazikon->naghshe[satr_ebteda][soton_ebteda][0]='K';
+                    //وصل کردن کشتی به لینک لیست اضافه شود
+                    gharar_dadan_w_atraf_keshtiha(bazikon);
+                }
+                else{
+                    printf("in mahal ghabel jay gozari nist.\n");
+                    goto lab1;
+                }
+            }
+            for (int j = 0; j <3 ; j++) {
+                lab2:
+                print_naghshe(bazikon->naghshe,0);
+                printf("kashti be tol 2:\n");
+                printf("\tsatre ebteda: ");
+                scanf("%d",&satr_ebteda);
+                getchar();
+                printf("\tsoton ebteda: ");
+                scanf("%d",&soton_ebteda);
+                getchar();
+                printf("\tsatre enteha: ");
+                scanf("%d",&satr_enteha);
+                getchar();
+                printf("\tsoton enteha: ");
+                scanf("%d",&soton_enteha);
+                getchar();
+                if (ghabel_jaygozari_bodan_kashtiha(bazikon,satr_ebteda,soton_ebteda,satr_enteha,soton_enteha,2)){
+                    bazikon->naghshe[satr_ebteda][soton_ebteda][0]='K';
+                    bazikon->naghshe[satr_enteha][soton_enteha][0]='K';
+                    // وصل کردن کشتی به لینک لیست اضافه شود
+                    gharar_dadan_w_atraf_keshtiha(bazikon);
+                }
+                else{
+                    printf("in mahal ghabel jay gozari nist.\n");
+                    goto lab2;
+                }
+            }
+            for (int k = 0; k <2 ; k++) {
+                lab3:
+                print_naghshe(bazikon->naghshe,0);
+                printf("kashti be tol 3:\n");
+                printf("\tsatre ebteda: ");
+                scanf("%d",&satr_ebteda);
+                getchar();
+                printf("\tsoton ebteda: ");
+                scanf("%d",&soton_ebteda);
+                getchar();
+                printf("\tsatre enteha: ");
+                scanf("%d",&satr_enteha);
+                getchar();
+                printf("\tsoton enteha: ");
+                scanf("%d",&soton_enteha);
+                getchar();
+                if (ghabel_jaygozari_bodan_kashtiha(bazikon,satr_ebteda,soton_ebteda,satr_enteha,soton_enteha,3)){
+                    if (satr_enteha == satr_ebteda){
+                        for (int i = minimum(soton_ebteda,soton_enteha); i <minimum(soton_ebteda,soton_enteha)+3 ; i++) {
+                            bazikon->naghshe[satr_ebteda][i][0]='K';
+                        }
+                    }
+                    else{
+                        for (int i = minimum(satr_ebteda,satr_enteha); i <minimum(satr_ebteda,satr_enteha)+3 ; i++) {
+                            bazikon->naghshe[i][soton_enteha][0]='K';
+                        }
+                    }
+                    gharar_dadan_w_atraf_keshtiha(bazikon);
+                }
+                else{
+                    printf("in mahal ghabel jay gozari nist.\n");
+                    goto lab3;
+                }
+            }
+            for (int l = 0; l <1 ; l++) {
+                lab4:
+                print_naghshe(bazikon->naghshe,0);
+                printf("kashti be tol 5:\n");
+                printf("\tsatre ebteda: ");
+                scanf("%d",&satr_ebteda);
+                getchar();
+                printf("\tsoton ebteda: ");
+                scanf("%d",&soton_ebteda);
+                getchar();
+                printf("\tsatre enteha: ");
+                scanf("%d",&satr_enteha);
+                getchar();
+                printf("\tsoton enteha: ");
+                scanf("%d",&soton_enteha);
+                getchar();
+                if (ghabel_jaygozari_bodan_kashtiha(bazikon,satr_ebteda,soton_ebteda,satr_enteha,soton_enteha,5)){
+                    if (satr_enteha==satr_ebteda){
+                        for (int i = minimum(soton_ebteda,soton_enteha); i <minimum(soton_ebteda,soton_enteha)+5 ; i++) {
+                            bazikon->naghshe[satr_ebteda][i][0]='K';
+                        }
+                    }
+                    else{
+                        for (int i = minimum(satr_ebteda,satr_enteha); i <minimum(satr_ebteda,satr_enteha)+5 ; i++) {
+                            bazikon->naghshe[i][soton_enteha][0]='K';
+                        }
+                    }
+                    gharar_dadan_w_atraf_keshtiha(bazikon);
+                }
+                else{
+                    printf("in mahal ghabel jay gozari nist.\n");
+                    goto lab4;
+                }
+            }
+
+
+    }
 }
 
 
@@ -158,6 +340,8 @@ void play_with_friend(FILE*user){
     player1=chose_user(user);
     create_board(&player1);
     sakhte_keshtiha(&player1);
+    put_ships(&player1);
+    print_naghshe(player1.naghshe,0);
 
 
 }
